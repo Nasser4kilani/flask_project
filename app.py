2763b9fb82a372719d669ce12e87fb53231b6968
kilanibarhoom <nkilani833@gmail.com>
2025-04-10 12:25:13 -0700

    initial

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+@app.route('/')
+def root():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True,port=8000)
